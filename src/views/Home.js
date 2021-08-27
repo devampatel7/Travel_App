dd7b1fcf63de8dcd910cb1d2332bc4e9ae0cf92d
Devam Patel <devampatel7@gmail.com>
2021-08-27 01:00:41 -0400

    first commit, nav bar and cover set up

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import { ReactDOM } from "react-dom";
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <main className='cover'>
+        <div>
+            <h1>Numinous.</h1>
+            <h1>Let us inspire you</h1>
+        </div>
+
+
+        </main>
+    )
+}
+
+export default Home;
